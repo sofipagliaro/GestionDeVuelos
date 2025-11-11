c916fb238cb0590709e51652af153eb83cceda1d
sofipagliaro <pagliarosofiaceleste@gmail.com>
2025-11-10 22:01:07 -0300

    implementar interfaz

--- src/clases/Pasajero.java
+++ src/clases/Pasajero.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.HashMap;
 
 public class Pasajero extends Persona implements I_VerViajes {
-    private boolean assistCard;
-    private boolean priorityPass;
     private List<Reserva> reservas;
 
     /// CONSTRUCTOR
@@ -16,7 +14,13 @@ public class Pasajero extends Persona implements I_VerViajes {
         this.reservas = new ArrayList<>();
     }
 
+    @Override
+    public List<Vuelo> verVuelos() {
+        return List.of();
+    }
 
-
-
+    @Override
+    public List<Reserva> verReservas() {
+        return List.of();
+    }
 }
